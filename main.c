67cee2a6bf6b581356b3bc7f299010bcfceb9a7c
hamzamtr <hamzamoutiri05@gmail.com>
2023-04-25 16:08:38 +0000

    task

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <limits.h>
 #include <stdio.h>
-#include "main.c"
+#include "main.h"
 
 /**
  * main - Entry point
@@ -39,6 +39,6 @@ len2 = printf("Percent:[%%]\n");
 _printf("Len:[%d]\n", len);
 printf("Len:[%d]\n", len2);
 _printf("Unknown:[%r]\n");
-printf("Unknown:[%r]\n");
+_printf("Unknown:[%r]\n");
 return (0);
 }
